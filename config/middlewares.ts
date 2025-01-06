ce9dbe412cbca41bc14899504a3921f6433eaf26
Rashid <85874561+rashid-090@users.noreply.github.com>
2025-01-06 16:54:48 +0530

    Update middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,45 @@
-export default [
-  'strapi::logger',
+module.exports = [
+ {
+    name: "strapi::body",
+    config: {
+      jsonLimit: "20mb",
+      formLimit: "20mb",
+      textLimit: "20mb",
+      formidable: {
+        maxFileSize: 20 * 1024 * 1024,
+      },
+    },
+  },
+]
+
+
+
+module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'dl.airtable.com', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
+
+
+
