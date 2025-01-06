c21958167a52439f67549644c2fac70d6469ce2c
Rashid <85874561+rashid-090@users.noreply.github.com>
2025-01-06 16:55:41 +0530

    Update plugins.ts

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,22 +4,21 @@ export default ({ env }) => ({
             jwtSecret: env('JWT_SECRET'),
         },
     },
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-                 localServer: {
-                      url: env('MEDIA_BASE_URL', '/uploads/'), // Default for local uploads
-                    },
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
+      upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
         },
+        actionOptions: {
+            upload: {
+                folder: 'dostudio-backend', // Specify the folder name here
+              },
+          uploadStream: {},
+          delete: {},
+        },
+      },
     },
 });
